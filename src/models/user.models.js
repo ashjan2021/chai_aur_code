1240d9578f32659365b98246e54483fb74a56986
Ashish Jangid <ashishjangid2021@gmail.com>
2025-09-29 18:22:34 +0530

    added userModel and videoModel

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
-    userSchema: {
+    username: {
         type: String,
         unique: true,
-        required: true,
+        required: true, 
         lowercase: true,
         trim: true,
         index: true // searching
@@ -45,5 +47,48 @@ const userSchema = mongoose.Schema({
     }
 }, { timeStamps: true }) // for created At and Updated At.
 
+
+//  mongoose hooks - pre - when save event happens encrypt the password before it is being saved.
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password) // returns true or false
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName 
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName 
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 module.exports = mongoose.model('User', userSchema);
